1cd267a29562d00d62e8363074825c9db0c47344
ElDonatello <47677152+ElDonatello@users.noreply.github.com>
2020-12-08 18:45:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,16 @@ else{
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J0B58GXX1S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J0B58GXX1S');
+</script>
+	
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -62,19 +72,9 @@ else{
 <link rel="stylesheet" href="./css/style.css">
  <!-- Responsive css files-->
  <link rel="stylesheet" href="./css/responsive.css">
-	
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-J0B58GXX1S"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-J0B58GXX1S');
-</script>
-	
+		
 </head>
-<script></script>
+
 <body>
 <!-- Start Header Area-->
 <header class="header_area">
